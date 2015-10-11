626424d344e1eb2ff3f6f68deeb6900d8f7ad4d5
laichuanfeng <laichuanfeng@hotmail.com>
2015-10-11 20:44:32 +0800

    可复用组件与混入

--- example/build/helloworld.js
+++ example/build/helloworld.js
@@ -1,7 +1,4 @@
-'use strict';
-
-React.render(React.createElement(
-  'h1',
-  null,
-  'Hello, world!'
-), document.getElementById('example'));
+React.render(
+  React.createElement("h1", null, "Hello, world!"),
+  document.getElementById('example')
+);
