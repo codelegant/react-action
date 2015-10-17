39fa11920839ae3b08c944678c66227776aaab43
Elegant Lai <elegant.lai@kiwifi.cn>
2015-10-17 10:35:36 +0800

    Transtion

---
+++ example/build/transition.js
@@ -0,0 +1,38 @@
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+
+var TodoList = React.createClass({displayName: "TodoList",
+  getInitialState: function() {
+    return {items: ['hello', 'world', 'click', 'me']};
+  },
+  handleAdd: function() {
+    var newItems =
+      this.state.items.concat([prompt('Enter some text')]);
+    this.setState({items: newItems});
+  },
+  handleRemove: function(i) {
+    var newItems = this.state.items;
+    newItems.splice(i, 1);
+    this.setState({items: newItems});
+  },
+  render: function() {
+    var items = this.state.items.map(function(item, i) {
+      return (
+        React.createElement("div", {key: item, onClick: this.handleRemove.bind(this,i)}, 
+          item
+        )
+      );
+    }.bind(this));
+    return (
+      React.createElement("div", null, 
+        React.createElement("button", {onClick: this.handleAdd}, "Add Item"), 
+        React.createElement(ReactCSSTransitionGroup, {transitionName: "example"}, 
+          items
+        )
+      )
+    );
+  }
+});
+React.render(
+React.createElement(TodoList, null),
+document.getElementById("example")
+	);
