11d13d9645cb456e02110f9c693c48af9dccc148
codelegant <laichuanfeng@hotmail.com>
2016-08-31 18:02:52 +0800

    UPDATE webpack config

--- webpack.pub.config.js
+++ webpack.pub.config.js
@@ -26,10 +26,7 @@ module.exports = {
       {
         test   : /\.js?$/,
         loader : 'babel',// short for babel-loader
-        exclude: /node_modules/,
-        query  : {
-          presets: ['react', 'es2015', 'stage-0']
-        }
+        exclude: /node_modules/
 
       },
       {
